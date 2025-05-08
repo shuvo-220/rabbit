c83b5ddf46d991522e4c3bf30b95f43b9cecb0af
shuvo-220 <shuvo.info220@gmail.com>
2025-05-08 20:26:40 +0800

    working on product route

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const protect = async(req, res, next)=>{
+    let token;
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+        try {
+            token = req.headers.authorization.split(" ")[1];
+            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+            req.user = await User.findById(decoded.user.id).select('-password');
+            next();
+        } catch (error) {
+            console.log('Token varification failed : ',error.message);
+            res.status(400).json({message:'no authorized'})
+        }
+    }else{
+        res.status(400).json({message:'token not available'});
+    }
+}
+
+module.exports = { protect }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
+const {protect} = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
@@ -31,5 +32,42 @@ router.post('/register', async(req, res)=>{
         res.status(500).send(error.message)
     }
 });
+
+//login
+router.post('/login', protect, async(req, res)=>{
+    const{email, password} = req.body;
+    try {
+        let user = await User.findOne({email})
+        if(!user){
+            res.status(400).json({message:'User not registered'});
+        }
+        const isMatch = await user.comparePassword(password);
+        if(!isMatch) return res.status(400).json({message:'invalid credentials'});
+
+        const payload = {user:{id:user._id, role:user.role}}
+
+        jwt.sign(payload, process.env.JWT_SECRET,{expiresIn:'40h'}, (err, token)=>{
+            if(err) throw err
+
+            res.status(201).json({
+                user:{
+                    _id:user._id,
+                    name:user.name,
+                    email:user.email,
+                    role:user.role
+                },
+                token
+            })
+        })
+    } catch (error) {
+        res.status(500).json(error.message);
+    }
+})
+
+router.get('/profile', protect, async(req, res)=>{
+    res.status(201).json(req.user);
+})
+
+
 
 module.exports = router;
