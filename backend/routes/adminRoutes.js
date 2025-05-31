89ea4ba00f717238a0873a39f3e332070fb0dec4
shuvo-220 <shuvo.info220@gmail.com>
2025-05-31 23:16:42 +0800

    work on user update

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -14,3 +14,34 @@ router.get('/', protect, admin, async(req, res)=>{
 })
 
 module.exports = router;
+
+
+router.post('/', protect, admin, async(req, res)=>{
+    const{name,email,password,role} = req.body;
+    try {
+        let user = await User.findOne({email});
+        if(user){
+            res.status(400).json({message:'user already exist'});
+        }
+        user = new User({
+            name,email,password,role:role || 'customer'
+        })
+        await user.save();
+        res.status(200).json({message:'User created successfully', user});
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+
+router.put('/:id', protect, admin, async(req, res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+
+
+
+
